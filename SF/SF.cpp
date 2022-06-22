284ddbae42ad2020fcf98632a8a4453243d5ed62
Illia Stetsenko <drumin123451@gmail.com>
2022-06-22 16:06:47 +0300

    not working version

--- SF/SF.cpp
+++ SF/SF.cpp
@@ -1,33 +1,66 @@
 #include "SF.h"
-
-
-std::u8string SF::ScanRootDirectory(const std::u8string &file_name) {
-    //Scan the root directory to find all the subdirectories
-    for (auto &it: iterator(root)) {
-        if (is_directory(it.path())) {
-            root_subdirs.push_back(it.path());
+namespace Operator {
+    namespace {
+        // function which scans directory and searches for the file name
+        void ScanDirectory(const_path &path, const std::u8string &file_name) {
+            //implementation
+            for (auto &it: iterator(path)) {
+                if(OperateDirectory(path, file_name)) break;
+            }
         }
-        //If we find the answer here, we don't have to initialize the threads
-        else if (it.path().filename() == file_name) {
-            return it.path().u8string();
+        bool OperateDirectory(const_path &path_, const std::u8string &file_name) {
+            std::unique_lock<std::mutex> lock;
+            lock.lock();
+            if(!path_function_result.empty()) return true;
+            lock.unlock();
+            std::vector<path> subdirectories;
+            for(const_path& _path: iterator(path_)){
+                if(is_directory(_path)){
+                    subdirectories.push_back(_path);
+                }
+                if(_path.u8string() == file_name){
+                    lock.lock();
+                    path_function_result = _path.u8string();
+                    return true;
+                }
+            }
+            if(std::ranges::any_of(subdirectories.begin(), subdirectories.end()
+                              ,[&file_name](const_path & _path) { return OperateDirectory(_path, file_name);})){
+                return true;
+            }
+            return false;
         }
     }
-    return {};
-}
-//just initialize root_deirectory, which then we are going to analise
-SF::SF(const std::u8string &root_directory):root{root_directory} {}
 
-std::u8string SF::ScanDirectory(const_path &path, const std::u8string &file_name) {
-    return u8"";
-}
+// we needn't use threads here, main thread is enough
+    std::u8string SF::ScanRootDirectory(const std::u8string &file_name) {
+        //Scan the root directory to find all the subdirectories
+        for (auto &it: iterator(root)) {
+            if (is_directory(it.path())) {
+                root_subdirs.push_back(it.path());
+            }
+                //If we find the answer here, we don't have to initialize the threads
+            else if (it.path().filename() == file_name) {
+                return it.path().u8string();
+            }
+        }
+        return {};
+    }
 
-std::u8string SF::FindPath(const std::u8string &file_name) {
-    auto result = ScanRootDirectory(file_name);
-    if(!result.empty()) return result;
+//just initialize the root_directory, which then we are going to analise
+    SF::SF(const std::u8string &root_directory) : root{root_directory} {}
 
-    ThreadPool pool(static_cast<int>(root_subdirs.size()));
-    pool.init();
-//    for(const_path& p: root_subdirs){
-//        auto ft = pool.submit(ScanRootDirectory, this, p, file_name);
-//    }
+    std::u8string SF::FindPath(const std::u8string &file_name) {
+        auto res = ScanRootDirectory(file_name);
+        if (!res.empty()) return res;
+        // now we need to initialize threads
+        ThreadPool pool(static_cast<int>(root_subdirs.size()));
+        pool.init();
+        //make tests to find out the best threads number
+        for (const_path &path_: root_subdirs) {
+            auto future = pool.submit(&Operator::ScanDirectory, path_, file_name);
+            future.get();
+        }
+        return Operator::path_function_result;
+    }
 }
--- SF/SF.h
+++ SF/SF.h
@@ -2,24 +2,40 @@
 
 #include <string>
 #include <filesystem>
+#include <algorithm>
 #include "../ThreadPool/Headers/ThreadPool.h"
+namespace Operator{
 
-class SF{
-private:
     using iterator = std::filesystem::directory_iterator;
+
     using path = std::filesystem::path;
+
     using const_path = const std::filesystem::path;
+    namespace {
+        std::mutex res_mutex;
+        std::u8string path_function_result{};
+        void ScanDirectory(const_path &path, const std::u8string &file_name);
+        bool OperateDirectory(const_path& path, const std::u8string& file_name);
+    }
 
-    path root;
-    std::vector<path> root_subdirs;
-    std::u8string ScanDirectory(const_path& path, const std::u8string& file_name);
-    std::u8string ScanRootDirectory(const std::u8string &file_name);
-public:
-    //the threads number will be default
+    class SF{
+    private:
+        // root directory
+        path root;
+        // root's subdirectories
+        std::vector<path> root_subdirs;
+        // function which scat the root directory
+        std::u8string ScanRootDirectory(const std::u8string &file_name);
+    public:
+        //the threads number will be default
+        explicit SF(const std::u8string& root_directory);
+        //we don't need a default constructor
+        SF() = delete;
+        // also a copy constructors
+        SF(const SF& copy) = delete;
+        SF(SF&& copy) = delete;
+        //the function which will do the job
+        std::u8string FindPath(const std::u8string& file_name);
+    };
+}
 
-    SF(const std::u8string& root_directory);
-    SF() = delete;
-    SF(const SF& copy) = delete;
-    SF(SF&& copy) = delete;
-    std::u8string FindPath(const std::u8string& file_name);
-};
